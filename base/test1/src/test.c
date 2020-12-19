4b32dce3355e7ad642bbf4a96647ad5efdf90b69
U-DESKTOP-F52D737\wancat <1264909603@qq.com>
2020-12-19 20:53:00 +0800

    add part 1

--- base/test1/src/test.c
+++ base/test1/src/test.c
@@ -1,20 +1,11 @@
 
+#include "param.h"
 
-
-#define MIN_PARAM_NUM 3
-
-int chk_param(int argc,char *argv[]){
-	
-	if(argc<MIN_PARAM_NUM){
-		return 0;
-	}else{
-		return 1;
-	}
-
-}
+int chk_param(int argc,char *argv[]);
 
 int main(int argc ,char *argv[]){
 
 	return chk_param(argc,argv);
 	
 }
+
